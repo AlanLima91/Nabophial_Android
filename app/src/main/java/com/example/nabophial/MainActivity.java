9b39077f5d393d1d550a0928eb831e0ba5ed7442
Alan <m.limaalan@hotmail.fr>
2019-05-03 19:24:57 +0200

    add Signup Activity and Connexion redirect to it

--- app/src/main/java/com/example/nabophial/MainActivity.java
+++ app/src/main/java/com/example/nabophial/MainActivity.java
@@ -1,50 +1,53 @@
 package com.example.nabophial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    private EditText    email;
+    private EditText    password;
+    private TextView    signup;
+    private TextView    forgetPass;
+    private Button      loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email       = (EditText) findViewById(R.id.email);
+        password    = (EditText) findViewById(R.id.password);
+        signup      = (TextView) findViewById(R.id.signup);
+        forgetPass  = (TextView) findViewById(R.id.forget_password);
+        loginBtn    = (Button) findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (login())
+                    openHomepage();
+                else
+                    return ;
+            }
+        });
     }
 
-    /**
-     *
-     * C'est cette fonction qui injecte la bar de menu dans la bar d'action
-     * sans cette methode pas de bar de menu (le menu "...")
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
+    public boolean login()
     {
-        getMenuInflater().inflate( R.menu.main_menu, menu );
-        return true;
+        return true ;
     }
 
-    /**
-     *
-     * Il permet de dire ce qui va se passer selon l'item sur lequel on clique
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
+    public void openHomepage()
     {
-        switch ( item.getItemId() ) // On récupère l'id de l'item cliqué
-        {
-            case R.id.menu_other: // Cas ou on clique sur l'item autre
-                Toast.makeText( this, "Other menu selected", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.menu_settings: // Cas ou on clique sur l'item parametre
-                Toast.makeText( this, "Settings menu selected", Toast.LENGTH_SHORT).show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+        // Not the right Activity, made it just for the test and it works
+        startActivity(new Intent(this, SignupActivity.class));
     }
+
 }
