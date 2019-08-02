984752a7d5b80b0c6b9a19aeebbf20bb5bf6698d
Alan <m.limaalan@hotmail.fr>
2019-08-03 00:51:38 +0200

    City

---
+++ app/src/main/java/nabophial/Model/City.java
@@ -0,0 +1,42 @@
+package nabophial.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class City {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("departement")
+    @Expose
+    private Departement departement;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Departement getDepartement() {
+        return departement;
+    }
+
+    public void setDepartement(Departement departement) {
+        this.departement = departement;
+    }
+
+}
