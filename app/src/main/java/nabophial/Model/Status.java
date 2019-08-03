27fdd57d2d7f7dea0c7edfd69ece0317bad18cc9
Alan <m.limaalan@hotmail.fr>
2019-08-03 19:53:32 +0200

    Model Status

---
+++ app/src/main/java/nabophial/Model/Status.java
@@ -0,0 +1,47 @@
+package nabophial.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Status {
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public Status() {
+    }
+
+    /**
+     *
+     * @param id
+     * @param name
+     */
+    public Status(Integer id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
