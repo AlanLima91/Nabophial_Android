24c02786d5b16e0c21574850cd42ac9971dd2e23
DevShimi <hospitalier.philippe@gmail.com>
2019-10-09 16:51:21 +0200

    Function for Sign In and communicate with API

--- app/src/main/java/nabophial/Network/RetrofitInstance.java
+++ app/src/main/java/nabophial/Network/RetrofitInstance.java
@@ -6,6 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitInstance {
 
     private static Retrofit retrofit;
+	
     private static final String BASE_URL = "https://nabophial.herokuapp.com/";
 
     public static Retrofit getRetrofitInstance(){
