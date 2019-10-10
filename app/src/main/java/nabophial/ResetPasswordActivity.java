3c70aa5c7e92f3129182c23ff214915685a34d4b
DevShimi <hospitalier.philippe@gmail.com>
2019-10-10 15:57:00 +0200

    Show page correctly

--- app/src/main/java/nabophial/ResetPasswordActivity.java
+++ app/src/main/java/nabophial/ResetPasswordActivity.java
@@ -7,6 +7,8 @@ public class ResetPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
     }
 }
