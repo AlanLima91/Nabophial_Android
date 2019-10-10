7e2a210e14dba3a70081a29ae82bb01048a3d354
Alan <m.limaalan@hotmail.fr>
2019-10-10 14:11:10 +0200

    Create Reset Password view

---
+++ app/src/main/java/nabophial/ResetPasswordActivity.java
@@ -0,0 +1,12 @@
+package nabophial;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
