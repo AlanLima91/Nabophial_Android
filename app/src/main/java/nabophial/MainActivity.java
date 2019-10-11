930c48a425fb5cb2ccaae610703e589e2873d963
DevShimi <hospitalier.philippe@gmail.com>
2019-10-11 14:54:44 +0200

    Update AuthToken to Session (and author file for same reason)
    And MainActivity for decode the token

--- app/src/main/java/nabophial/MainActivity.java
+++ app/src/main/java/nabophial/MainActivity.java
@@ -3,13 +3,19 @@ package nabophial;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import nabophial.Model.AuthToken;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.io.UnsupportedEncodingException;
+
+import nabophial.Model.Session;
 import nabophial.Network.RetrofitInstance;
 import nabophial.Network.RetrofitService;
 import retrofit2.Call;
@@ -17,11 +23,12 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private static RetrofitService client;
-    public String TokenNow;
+    public static Session session = new Session(null,"");
     private EditText email;
     private EditText password;
     private Button loginBtn;
@@ -41,19 +48,22 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(final View view) {
 
                 client = RetrofitInstance.getRetrofitInstance().create(RetrofitService.class); // On crée notre instance
-                Log.e(TAG, "Email : " + email.getText() + " Password : " + password.getText());//toString()+ password.toString()
-                Call<AuthToken> call = client.login(email.getText().toString(), password.getText().toString());
 
-                call.enqueue(new Callback<AuthToken>() {
+
+                Call<Session> call = client.login(email.getText().toString(), password.getText().toString());
+
+                call.enqueue(new Callback<Session>() {
 
                     @Override
-                    public void onResponse(Call<AuthToken> call, Response<AuthToken> response) {
+                    public void onResponse(Call<Session> call, Response<Session> response) {
 
-                        Log.e(TAG, "Code de réponse de l'api : " + response.code());
+                        Log.i(TAG, "Code de réponse de l'api : " + response.code());
 
                         if (response.isSuccessful()) {
-                            Log.i("Token : ", response.body().toString());
-                            TokenNow = response.body().getToken();
+                            session.setToken(response.body().getToken());
+                            DecodeId(session.getToken());
+                            Log.i(" ID : ", session.getId().toString());
+                            Log.i(" Token : ", session.getToken());
                             openHomepage(view);
                         } else {
                             Toast.makeText(getApplicationContext(), "Identifiant incorrect", Toast.LENGTH_LONG).show();
@@ -61,8 +71,8 @@ public class MainActivity extends AppCompatActivity {
                     }
 
                     @Override
-                    public void onFailure(Call<AuthToken> call, Throwable t) {
-                        Log.e(TAG, "FAUX onResponse: ConfigurationListener::" + call.request().url());
+                    public void onFailure(Call<Session> call, Throwable t) {
+                        Log.e(TAG, "Erreur onFailure: ConfigurationListener::" + call.request().url());
                         Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
                     }
                 });
@@ -71,15 +81,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void SignUp(View view) {
+
         startActivity(new Intent(this, SignupActivity.class));
     }
 
     public void ResetPassword(View view) {
+
         startActivity(new Intent(this, ResetPasswordActivity.class));
     }
 
     public void openHomepage(View view) {
+
         startActivity(new Intent(this, HomeActivity.class));
     }
+
+    private void DecodeId(String token) {
+
+        try {
+            String[] split = token.split("\\.");
+
+            //Log.i("JWT_DECODED", "Header: " + getJson(split[0]));
+            //Log.i("JWT_DECODED", "Body: " + getJson(split[1]));
+
+            JsonObject convertedObject = new Gson().fromJson(getJson(split[1]), JsonObject.class);
+
+            session.setId(convertedObject.get("id").getAsInt());
+
+
+        } catch (UnsupportedEncodingException e) {
+
+        }
+        //Log.i(" ID : ", jwt.getId());
+        //session.setId(Integer.parseInt(jwt.getId()));
+    }
+
+    private static String getJson(String strEncoded) throws UnsupportedEncodingException {
+            byte[] decodedBytes = Base64.decode(strEncoded, Base64.URL_SAFE);
+            return new String(decodedBytes, "UTF-8");
+    }
+
 
 }
